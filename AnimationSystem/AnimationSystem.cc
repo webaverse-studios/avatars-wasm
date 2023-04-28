4aa61b2c2bbc736ac14f394d181ef0a92e3e1501
gonnavis <gonnavis@163.com>
2023-04-28 23:08:11 +0800

    Fix `rand()` bugs.

--- AnimationSystem/AnimationSystem.cc
+++ AnimationSystem/AnimationSystem.cc
@@ -909,11 +909,11 @@ namespace AnimationSystem {
           // }
 
           avatar->lastRandomSittingIdleStartTimeS = AnimationMixer::nowS;
-          avatar->lastRandomIntervalTimeS = rand() % 13 + 3; // 3 ~ 15
-          avatar->lastRandomSpeed = rand() % 76 / 100 + 0.25; // 0.25 ~ 1
+          avatar->lastRandomIntervalTimeS = (rand() % 13) + 3; // 3 ~ 15
+          avatar->lastRandomSpeed = float(rand() % 76) / 100 + 0.25; // 0.25 ~ 1
           std::cout << "lastRandomSittingIdleIndex: " << avatar->lastRandomSittingIdleIndex << std::endl;
-          std::cout << "lastRandomSpeed: " << avatar->lastRandomSittingIdleIndex << std::endl;
-          std::cout << "lastRandomIntervalTimeS: " << avatar->lastRandomSittingIdleIndex << std::endl;
+          std::cout << "lastRandomSpeed: " << avatar->lastRandomSpeed << std::endl;
+          std::cout << "lastRandomIntervalTimeS: " << avatar->lastRandomIntervalTimeS << std::endl;
           std::cout << "---" << std::endl;
         }
       }
