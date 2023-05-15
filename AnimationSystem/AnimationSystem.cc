af6b5957755ea6f0ef8d6e58c1587294e8fddbd3
gonnavis <gonnavis@163.com>
2023-05-15 08:05:54 +0800

    Only blend top bones, keep hips and legs animation of default idle.

--- AnimationSystem/AnimationSystem.cc
+++ AnimationSystem/AnimationSystem.cc
@@ -820,16 +820,19 @@ namespace AnimationSystem {
   float *_blendIdle(AnimationMapping &spec, Avatar *avatar) {
     float *v1 = evaluateInterpolant(animationGroups[animationGroupIndexes.Single][singleAnimationIndexes.Idle], spec.index, fmod(avatar->timeSinceLastMoveS + avatar->idleBias * animationGroups[animationGroupIndexes.Single][singleAnimationIndexes.Idle]->duration, animationGroups[animationGroupIndexes.Single][singleAnimationIndexes.Idle]->duration));
    
-    Animation *randomIdleAnimation = animationGroups[animationGroupIndexes.RandomIdle][avatar->randomIdleAnimationIndex];
-    float timeS = avatar->randomIdleTime / 1000;
-    float t2 = min(timeS, avatar->randomIdleDuration);
-    float *v2 = evaluateInterpolant(randomIdleAnimation, spec.index, t2 * avatar->randomIdleSpeed);
+    if (spec.isTop) {
+      Animation *randomIdleAnimation = animationGroups[animationGroupIndexes.RandomIdle][avatar->randomIdleAnimationIndex];
+      float timeS = avatar->randomIdleTime / 1000;
+      float t2 = min(timeS, avatar->randomIdleDuration);
+      float *v2 = evaluateInterpolant(randomIdleAnimation, spec.index, t2 * avatar->randomIdleSpeed);
 
-    float f0 = t2 / 0.2;
-    float f1 = (avatar->randomIdleDuration - t2) / 0.2;
-    float f = min(f0, f1);
-    f = min(1, f);
-    interpolateFlat(v1, 0, v1, 0, v2, 0, f, spec.isPosition);
+      float f0 = t2 / 0.2;
+      float f1 = (avatar->randomIdleDuration - t2) / 0.2;
+      float f = min(f0, f1);
+      f = min(1, f);
+      interpolateFlat(v1, 0, v1, 0, v2, 0, f, spec.isPosition);
+    }
+    
     return v1;
   }
 
